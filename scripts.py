f408979f684635bb35f6e7aced6767daf11e16fe
Polina <75391420+wezbicka@users.noreply.github.com>
2022-11-03 14:17:47 +0300

    5 step

--- scripts.py
+++ scripts.py
@@ -5,3 +5,15 @@ from datacenter.models import Schoolkid
 students = Schoolkid.objects.all()
 print(students)
 
+# шаг 5
+# Ivan = Schoolkid.objects.filter(full_name__contains="Фролов")
+# print(Ivan) 
+"""<QuerySet [<Schoolkid: Фролов Каллистрат Арсеньевич 4А>, 
+<Schoolkid: Фролов Иван Григорьевич 6А>, 
+<Schoolkid: Моисеев Терентий Фролович 6В>, 
+<Schoolkid: Горбачев Агап Фролович 9В>]>"""
+# i = Ivan.filter(full_name__contains="Иван")
+# child = Ivan.filter(full_name__contains="Иван")[0]
+child = Schoolkid.objects.filter(full_name__contains="Фролов Иван")[0]
+print(child.full_name) 
+

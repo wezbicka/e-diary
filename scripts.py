3b81ab62adbd67769babade461fa8cb82a3d2bde
Polina <75391420+wezbicka@users.noreply.github.com>
2022-11-05 13:23:42 +0300

    step 19

--- scripts.py
+++ scripts.py
@@ -148,3 +148,54 @@ Commendation.objects.create(
     subject=subject,
     teacher=teacher
 )
+
+# шаг 19
+import random
+
+
+def create_commendation(schoolkid, subject):
+    child_lesson = Lesson.objects.filter(
+        year_of_study=schoolkid.year_of_study, 
+        group_letter=schoolkid.group_letter,
+        subject__title=subject,
+    ).order_by('-date').first()
+    commendations = [
+        "Молодец!",
+        "Отлично!",
+        "Хорошо!",
+        "Гораздо лучше, чем я ожидал!",
+        "Ты меня приятно удивил!",
+        "Великолепно!",
+        "Прекрасно!",
+        "Ты меня очень обрадовал!",
+        "Именно этого я давно ждал от тебя!",
+        "Сказано здорово – просто и ясно!",
+        "Ты, как всегда, точен!",
+        "Очень хороший ответ!",
+        "Талантливо!",
+        "Ты сегодня прыгнул выше головы!",
+        "Я поражен!",
+        "Уже существенно лучше!",
+        "Потрясающе!",
+        "Замечательно!",
+        "Прекрасное начало!",
+        "Так держать!",
+        "Ты на верном пути!",
+        "Здорово!",
+        "Это как раз то, что нужно!",
+        "Я тобой горжусь!",
+        "С каждым разом у тебя получается всё лучше!",
+        "Мы с тобой не зря поработали!",
+        "Я вижу, как ты стараешься!",
+        "Ты растешь над собой!",
+        "Ты многое сделал, я это вижу!",
+        "Теперь у тебя точно все получится!",
+    ]
+    commendation = random.choice(commendations)
+    Commendation.objects.create(
+        text=commendation,
+        created=child_lesson.date,
+        schoolkid=schoolkid,
+        subject=child_lesson.subject,
+        teacher=child_lesson.teacher
+    )
